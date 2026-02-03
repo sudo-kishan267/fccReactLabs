ddb6aa0b2ca6940a771dd74d38445691a9ed87b7
Kishan <kishanmishra267@gmail.com>
2026-02-03 21:23:48 +0530

    Code Done

---
+++ ReusableFooter/index.jsx
@@ -0,0 +1,56 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="links">
+        
+        <ul>
+          <li>
+            <a href="">Fitness Dashboard</a>
+          </li>
+          <li>
+            <a href="">Watch Videos</a>
+          </li>
+        </ul>
+        
+        <ul>
+          <li>
+            <a href="">Discord</a>
+          </li>
+          <li>
+            
+             <a href="">Services</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <a href="">Privacy Policy</a>
+          </li>
+          <li>
+           <a href="">Terms & Conditions</a>
+          </li>        
+        </ul>
+
+      </div>
+      
+      <div className="copyright">
+        <p>© 2024 Fitness Dashboard. All Rights Reserved.</p>
+      </div>
+      
+      <div className="icons">
+        <li>
+          <a href="">🎮</a>
+        </li>
+        <li>
+          <a href="">🐦</a>
+        </li>
+        <li>
+          <a href="">💻</a>
+        </li>
+        <li>
+          <a href="">🏀</a>
+        </li>
+      </div>
+    </footer>
+  );
+};
