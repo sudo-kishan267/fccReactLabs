036c2fa510782444b30e114156590ff40ce1ba9d
Kishan <kishanmishra267@gmail.com>
2026-02-05 00:38:15 +0530

    Done

--- colorPickerApp/index.jsx
+++ colorPickerApp/index.jsx
@@ -1,20 +1,47 @@
 const { useState } = React;
 
 export const ColorPicker = () => {
-    const [currentColor , setCurrentColor] = useState("#ffffff");
+  const [currentColor, setCurrentColor] = useState("#ffffff");
 
+  const handleColorChange = (e) => {
+    console.log(e.target.value);
+    setCurrentColor(e.target.value);
+  };
 
-
-    const handleColorChange = (e)=> {
-            console.log(e.target.value);
-            setCurrentColor(e.target.value);
-    };
-    
-    
-    return (
-        <div id="color-picker-container" style={{ backgroundColor: currentColor  }}>
-            <p className="line">Choose a color from the box for the background.</p>
-            <input type="color" id="color-input" onChange={handleColorChange} />
-        </div>
-    );
+  return (
+    <div id="color-picker-container" style={{ backgroundColor: currentColor }}>
+      <p className="line">Choose a color from the box for the background.</p>
+      <input
+        id="color-input"
+        type="color"        
+        value={currentColor}
+        onChange={handleColorChange}
+      />
+    </div>
+  );
 };
+
+// //Without handleColorChange (inline)
+// const { useState } = React;
+
+// export const ColorPicker = () => {
+//   const [currentColor, setCurrentColor] = useState("#ffffff");
+
+//   return (
+//     <div
+//       id="color-picker-container"
+//       style={{ backgroundColor: currentColor }}
+//     >
+//       <p className="line">
+//         Choose a color from the box for the background.
+//       </p>
+
+//       <input
+//         id="color-input"
+//         type="color"
+//         value={currentColor}
+//         onChange={(e) => setCurrentColor(e.target.value)}
+//       />
+//     </div>
+//   );
+// };
